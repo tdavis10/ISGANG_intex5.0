241859cd1e1427ce29a153eb6b65ccfab4676b1a
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-04-06 21:29:46 -0600

    Added by Ryan

--- AuthLab2_RyanPinkney/Startup.cs
+++ AuthLab2_RyanPinkney/Startup.cs
@@ -60,7 +60,7 @@ namespace AuthLab2_RyanPinkney
             });
 
             services.AddSingleton<InferenceSession>(
-                  new InferenceSession("crash_final2.onnx")
+                  new InferenceSession("wwwroot/crash_final2.onnx")
                 );
 
 
