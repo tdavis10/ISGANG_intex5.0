93133af4a7718a87fb966c1e42903099b0d2cd46
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-04-07 21:27:08 -0600

    Added by Ryan

--- AuthLab2_RyanPinkney/Prediction.cs
+++ AuthLab2_RyanPinkney/Prediction.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿// Author: Ryan Pinkney, Tanner Davis, Kevin Gutierrez, Jacob Poor
+// This is the predicted value model
+using System;
 namespace AuthLab2_RyanPinkney
 {
     public class Prediction
     {
-
+        // The predicted value attribute
         public string PredictedValue { get; set; }
 
 
